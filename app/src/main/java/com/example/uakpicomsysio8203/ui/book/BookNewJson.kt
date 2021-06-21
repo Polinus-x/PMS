7595ff7e29cf7f69a41d5147356665f1cfabc8d7
Polinus-x <poliennaa@gmail.com>
2021-06-21 03:33:10 +0300

    pray for e-shka

---
+++ app/src/main/java/com/example/uakpicomsysio8203/ui/book/BookNewJson.kt
@@ -0,0 +1,39 @@
+package com.example.uakpicomsysio8203.ui.book
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+class BookNewJson {
+
+    @JsonProperty("title")
+    var title: String? = null
+
+    @JsonProperty("subtitle")
+    var subtitle: String? = null
+
+    @JsonProperty("authors")
+    var authors: String? = null
+
+    @JsonProperty("publisher")
+    var publisher: String? = null
+
+    @JsonProperty("isbn13")
+    var isbn13: String? = null
+
+    @JsonProperty("pages")
+    var pages: String? = null
+
+    @JsonProperty("year")
+    var year: String? = null
+
+    @JsonProperty("rating")
+    var rating: String? = null
+
+    @JsonProperty("desc")
+    var desc: String? = null
+
+    @JsonProperty("price")
+    var price: String? = null
+
+    @JsonProperty("image")
+    var image: String? = null
+}
